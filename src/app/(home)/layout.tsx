83122336c3b9abcb2ab44b071a8a868f91cd0a2c
smoorf123 <somil2004@gmail.com>
2024-08-28 01:40:40 -0400

    margin adjustments

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,45 +1,39 @@
-import type { Metadata } from "next"
-import Image from "next/image"
-import Link from "next/link"
-import "../globals.css"
-import Navbar from "@/components/navbar/Navbar"
-import logo from "../../../public/images/logo.png"
+import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import "../globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import logo from "../../../public/images/logo.png";
 
 export const metadata: Metadata = {
-	title: "CMSC398T",
-	description:
-		"CMSC398T: Beyond Aesthetics: Accessible Frontend Design",
-}
+  title: "CMSC398T",
+  description: "CMSC398T: Beyond Aesthetics: Accessible Frontend Design",
+};
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body>
-				<div className="w-max">
-					<div className="grid gap-y-12 pl-56 pr-60 pt-48">
-						<Link
-							className="w-max"
-							href="/"
-						>
-							<Image
-								className="hover:animate-custom-pulse w-18 col-start-1 col-end-1 row-start-1 row-end-1"
-								src={logo}
-								alt="CMSC398T logo"
-							/>
-						</Link>
-						<div className="col-start-1 col-end-1 row-start-2 row-end-2 w-[17.5rem]">
-							<Navbar />
-						</div>
-						<div className="col-start-2 col-end-2 row-start-2 row-end-2 flex">
-							{children}
-						</div>
-					</div>
-				</div>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <body className="w-max pl-[25%] pr-[50%] pt-[17%]">
+        <div className="grid gap-y-12">
+          <Link className="w-max" href="/">
+            <Image
+              className="hover:animate-custom-pulse w-18 col-start-1 col-end-1 row-start-1 row-end-1"
+              src={logo}
+              alt="CMSC398T logo"
+            />
+          </Link>
+          <div className="col-start-1 col-end-1 row-start-2 row-end-2 w-[17.5rem]">
+            <Navbar />
+          </div>
+          <div className="col-start-2 col-end-2 row-start-2 row-end-2 flex">
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
 }
--- src/app/(subpages)/layout.tsx
+++ src/app/(subpages)/layout.tsx
@@ -1,47 +1,43 @@
-import type { Metadata } from "next"
-import Image from "next/image"
-import Link from "next/link"
-import "../globals.css"
-import Navbar from "@/components/navbar/Navbar"
-import logo from "../../../public/images/logo.png"
+import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import "../globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import logo from "../../../public/images/logo.png";
 
 export const metadata: Metadata = {
-	title: "CMSC398T",
-	description:
-		"CMSC398T: Beyond Aesthetics: Accessible Frontend Design",
-}
+  title: "CMSC398T",
+  description: "CMSC398T: Beyond Aesthetics: Accessible Frontend Design",
+};
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className="flex">
-				<div className="w-max">
-					<div className="grid gap-y-12 pl-56 pt-48">
-						<Link
-							className="w-max"
-							href="/"
-						>
-							<Image
-								className="block hover:animate-custom-pulse w-18 col-start-1 col-end-1 row-start-1 row-end-1"
-								src={logo}
-								alt="CMSC398T logo"
-							/>
-						</Link>
-						<div className="col-start-1 col-end-1 row-start-2 row-end-2 w-64">
-							<Navbar />
-						</div>
-					</div>
-				</div>
-				<div className="relative overflow-hidden h-150 w-full">
-					<div className="absolute overflow-y-scroll w-full h-150 -right-6">
-						{children}
-					</div>
-				</div>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <body className="flex pl-[25%] pr-[50%] pt-[17%]">
+        <div className="w-max">
+          <div className="grid gap-y-12">
+            <Link className="w-max" href="/">
+              <Image
+                className="block hover:animate-custom-pulse w-18 col-start-1 col-end-1 row-start-1 row-end-1"
+                src={logo}
+                alt="CMSC398T logo"
+              />
+            </Link>
+            <div className="col-start-1 col-end-1 row-start-2 row-end-2 w-64">
+              <Navbar />
+            </div>
+          </div>
+        </div>
+        <div className="relative overflow-hidden h-150 pl-36 pr-36 -mt-24">
+          <div className="absolute overflow-y-scroll h-150 -right-6 ]">
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
 }
